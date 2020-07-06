a091ce7e211f18e9433604fc13e8f0f6fd6092aa
stepanenko <serhiy.stepanenko@aiesec.net>
2020-07-06 09:50:06 +0300

    observer in progress

--- Observer/main.js
+++ Observer/main.js
@@ -1,5 +1,32 @@
 
 const Task = require('./task');
 
+const notificationService = function () {
+  const message = 'Notiffying';
+  this.update = function (task) {
+    console.log(message, task.user, 'for task', task.name);
+  }
+};
+const loggingService = function () {
+  const message = 'Logging';
+  this.update = function (task) {
+    console.log(message, task.user, 'for task', task.name);
+  }
+};
+function ObserverList() {
+  this.observerList = [];
+}
+
+ObserverList.prototype.add = function(obj) {
+  return this.observerList.push(obj);
+};
+ObserverList.prototype.get = function(index) {
+  if (index > -1 && index < this.observerList.length) {
+    return this.observerList[index];
+  }
+};
+
+const ns = new notificationService();
+const ls = new loggingService();
 const task = new Task({ name: 'Observe the nature', user: 'Wyat' });
 task.save();
